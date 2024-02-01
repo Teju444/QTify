c6771384fa6a89ac61671908509da9e1e695707a
Teju444 <tejukgowda04@gmail.com>
2024-02-01 14:13:58 +0530

    Module 2

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </div>
-  )
+  );
 }
 
 export default App;
